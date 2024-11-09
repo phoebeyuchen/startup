0223dedb9bb9fb5526ed9babc478253dc42d9f78
Yu Chen <cy928@byu.edu>
2024-11-08 22:36:07 -0700

    create Progress

---
+++ startupVite/src/progress/progress.jsx
@@ -0,0 +1,47 @@
+import checklistIcon from '../assets/checklist.png';
+
+export default function Progress() {
+  const answers = [
+    {
+      id: 1,
+      question: "How did you feel when you first met each other?",
+      answers: {
+        Phoebe: "Awesome",
+        Spencer: "Great"
+      },
+      date: "Sep 2, 2024"
+    },
+  ];
+
+  return (
+    <main>
+      <div className="title">
+        <h2>Answered Questions</h2>
+        <img src={checklistIcon} alt="checklist" />
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Question</th>
+            <th>Answer</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {answers.map((answer) => (
+            <tr key={answer.id}>
+              <td>{answer.id}</td>
+              <td>{answer.question}</td>
+              <td>
+                Phoebe: {answer.answers.Phoebe}<br />
+                Spencer: {answer.answers.Spencer}
+              </td>
+              <td>{answer.date}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </main>
+  );
+}
