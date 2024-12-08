4caea3bbfde2fe6cb240633d6655a7d7be7fbc52
Yu Chen <cy928@byu.edu>
2024-12-07 21:33:48 -0700

    add the new component to the home page

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,10 +1,20 @@
-import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import heartIcon from '../assets/heart.png';
+import { PartnerConnection } from '../partner/partner';
 import './home.css';
 
 export default function Home({ setIsLoggedIn, userEmail }) {
   const username = userEmail.split('@')[0];
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    navigate('/');
+  };
+
+  const handleQuestion = () => {
+    navigate('/question');
+  };
 
   return (
     <main>
@@ -13,40 +23,16 @@ export default function Home({ setIsLoggedIn, userEmail }) {
         <img src={heartIcon} alt="heart" className="home-icon"/>
       </div>
       
+      <PartnerConnection />
+      
       <div className="question-logout">
-        <QuestionButton />
-        <LogoutButton setIsLoggedIn={setIsLoggedIn} />
+        <button onClick={handleQuestion} className="question">
+          See Today's Question
+        </button>
+        <button onClick={handleLogout} className="logout">
+          Logout
+        </button>
       </div>
-
     </main>
-  );
-}
-
-export function LogoutButton({ setIsLoggedIn }) {
-  const navigate = useNavigate();
-
-  const handleLogout = () => {
-    setIsLoggedIn(false);
-    navigate('/');
-  };
-
-  return (
-    <button onClick={handleLogout} className="logout">
-      Logout
-    </button>
-  );
-}
-
-function QuestionButton() {
-  const navigate = useNavigate();
-
-  const handleQuestion = () => {
-    navigate('/question');
-  };
-
-  return (
-    <button onClick={handleQuestion} className="question">
-      See Today's Question
-    </button>
   );
 }
